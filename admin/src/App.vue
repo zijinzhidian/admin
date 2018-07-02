b5d0b8c2e97c4dc7facde760b3a2e0d613639210
apple <593045640@qq.com>
2018-07-02 18:04:30 +0800

    no message

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <h3>哈哈哈哈</h3>
+    <todo-item></todo-item>
   </div>
 </template>
 
---
+++ admin/src/test.js
@@ -0,0 +1,5 @@
+console.log(456);
+import Vue from 'Vue'
+Vue.component('todo-item', {
+	template: '<li>item</li>'
+})
