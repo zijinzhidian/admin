3a27434b04dc50a3d5b942a3ae7ca55fb5eb7c24
DESKTOP-1BGD0FN\Administrator <593045640@qq.com>
2018-07-27 00:31:11 +0800

    no message

--- admin/src/views/dashboard/admin/components/TodoList/Todo.vue
+++ admin/src/views/dashboard/admin/components/TodoList/Todo.vue
@@ -4,13 +4,20 @@
 			<input 
 				class="toggle" 
 				type="checkbox"
-				@checked="todo.done"
+				:checked="todo.done"
 				@change="toggleTodo(todo)">
 			<!-- @dblclick:双击事件 -->
 			<label v-text="todo.text" @dblclick="editing = true"></label> 
 			<button class="destroy" @click="deleteTodo(todo)"></button>  
 		</div>
-		<!-- <input type="" name=""> -->
+		<input
+			class="edit"
+			v-show="editing"
+			:value="todo.text"
+			@keyup.enter="doneEdit"
+			@keyup.esc="cancelEdit"
+			@blur="doneEdit"
+		>
 	</li>
 </template>
 
@@ -24,11 +31,29 @@
 			}
 		},
 		methods: {
+			// 菜单状态的改变
 			toggleTodo(todo) {
-
+				this.$emit('toggleTodo', todo)
 			},
-			deleteTodo(tood) {
-
+			// 删除菜单
+			deleteTodo(todo) {
+				this.$emit('deleteTodo', todo)
+			},
+			// 完成编辑
+			doneEdit(e) {
+				const value = e.target.value.trim()		//输入的值
+				const { todo } = this									//通过解析赋值获取todo
+				if (!value) {			//无数据则删除
+					this.deleteTodo(todo)
+				} else {
+					// this.$emit
+					todo.text = value
+				} 
+				this.editing = false 
+			},
+			// 取消编辑
+			cancelEdit() {
+				this.editing = false
 			}
 		}
 	}
--- admin/src/views/dashboard/admin/components/TodoList/index.vue
+++ admin/src/views/dashboard/admin/components/TodoList/index.vue
@@ -6,12 +6,22 @@
 		</header>
 		<!-- mian section -->
 		<section class="main" v-show="todos.length">
-			<input id="toggle-all" class="toggle-all" type="checkbox" :checked="allChecked">
+			<input id="toggle-all" class="toggle-all" type="checkbox" :checked="allChecked" @change="toggleAll()">
 			<label for="toggle-all"></label>
 			<ul class="todo-list">
-				<todo v-for="(todo, index) in todos" :key="index" :todo="todo"></todo>
+				<todo @toggleTodo="toggleTodo" @editTodo="editTodo" @deleteTodo="deleteTodo" v-for="(todo, index) in todos" :key="index" :todo="todo"></todo>
 			</ul>
 		</section>
+		<!-- footer -->
+		<footer class="footer" v-show="todos.length">
+			<span class="todo=count">
+				<strong>{{ remaining }}</strong>
+				{{ remaining | pluralize('item') }} left
+			</span>
+			<ul class="filters">
+				<li v-for="(value, key)"></li>
+			</ul>
+		</footer>
 	</section>
 </template>
 
@@ -19,7 +29,6 @@
 <script>
 	import Todo from './Todo'
 
-	const STORAGE_KEY = 'todos'
 	const defaultList = [
 	  { text: 'star this repository', done: false },
 	  { text: 'fork this repository', done: false },
@@ -30,25 +39,61 @@
 	  { text: 'axios', done: true },
 	  { text: 'webpack', done: true }
 	]
+	const filters = {
+		all: todos => todos,			//全部
+		active: todos => todos.filter(todo => !todo.done),		//未完成的
+		compelted: todos => todos.filter(todo => todo.done)		//已完成的
+	}
 
 	export default {
 		components: { Todo },
 		data() {
 			return {
-				todos: defaultList
+				todos: defaultList,
+				filters
 			}
 		},
 		methods: {
-			// 添加新的菜单
+			// 添加菜单
 			addTodo(e) {
-
+				const text = e.target.value
+				if (text.trim()) {
+					this.todos.push({ text, done: false })
+				}
+				e.target.value = ''
+			},
+			// 删除菜单
+			deleteTodo(todo) {
+				this.todos.splice(this.todos.indexOf(todo), 1)
+			},
+			// 编辑菜单
+			editTodo({ todo, value }) {
+				todo.text = value
+			},
+			// 菜单状态的改变
+			toggleTodo(todo) {
+				todo.done = !todo.done
+			},
+			// 全选状态的改变
+			toggleAll() {
+				const done = !this.allChecked
+				this.todos.forEach(todo => {
+					todo.done = done
+				})
 			}
 		},
 		computed: {
 			// 是否全选
 			allChecked() {
 				return this.todos.every(todo => todo.done)
+			},
+			// 未完成的个数
+			remaining() {
+				return this.todos.filter(todo => !todo.done).length
 			}
+		},
+		filters: {
+			pluralize: (remaining, content) => remaining === 1 ? content : content + 's'
 		}
 	}
 </script>
